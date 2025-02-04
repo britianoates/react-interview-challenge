cb1f2e307bb012c4551e556363b587b1fec16bd1
Britian Oates <neostrider2003@gmail.com>
2025-02-04 17:37:55 -0600

    Adding a comment about the math for checking the credit limit. The numbers could be moved around but this felt like the cleanest way of capturing and doing the math with the negative values involved - Brit

--- api/handlers/withdrawValidator.ts
+++ api/handlers/withdrawValidator.ts
@@ -21,6 +21,8 @@ const withdrawlRules = [
     (account:any, transactions:Array<any>, amount:number) => {return amount % 5 != 0 ? "Amount not in $5 increments" : ""},
     (account:any, transactions:Array<any>, amount:number) => {return account.type == "checking" && account.amount < amount ? "Insufficient funds" : ""},
     (account:any, transactions:Array<any>, amount:number) => {return account.type == "savings" && account.amount < amount ? "Insufficient funds" : ""},
+    //credit check, we take the amount they already owe minus the additional amount they are requesting. this number is negative the total amount they would owe.
+    // By adding their credit limit (which is stored as a positve integer) we have a positive number they would be available to borrow after this transaction, or its been maxed
     (account:any, transactions:Array<any>, amount:number) => {return account.type == "credit" && account.amount - amount + account.credit_limit < 0 ? "Insufficient credit" : ""},
     (account:any, transactions:Array<any>, amount:number) => {
         const todaysDate = new Date().toDateString()
